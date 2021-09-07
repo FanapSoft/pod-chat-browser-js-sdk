d57def98b3670a0e8ce090dfc381af8ccca14e01
wp-src <wpmagic70@gmail.com>
2021-09-07 09:52:15 +0430

    Added kotern connectivity test function

--- src/chat.js
+++ src/chat.js
@@ -15574,6 +15574,55 @@
             asyncClient.logout();
         };
 
+        /**
+         * Test communication with kotern
+         *
+         * @param turnIp
+         * @param port
+         * @param useUDP
+         * @param username
+         * @param password
+         * @param timeout
+         * @return {Promise<boolean>}
+         */
+        this.checkTURNServer =  function (turnIp, port, useUDP = false, username = 'mkhorrami', password = 'mkh_123456', timeout) {
+            let url = 'turn:' + turnIp + ':' + port + '?transport=' + (useUDP ? 'udp' : 'tcp');
+            const turnConfig = {
+                urls: url,
+                username: username,
+                credential: password
+            }
+            console.log('turnConfig: ', turnConfig);
+            return new Promise(function (resolve, reject) {
+
+                let promiseResolved;
+                setTimeout(function () {
+                    if (promiseResolved) return;
+                    resolve(false);
+                    promiseResolved = true;
+                }, timeout || 5000);
+
+                promiseResolved = false;
+                let myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection //compatibility for firefox and chrome
+                    , pc = new myPeerConnection({iceServers: [turnConfig]}),
+                    noop = function () {
+                    };
+                pc.createDataChannel(""); //create a bogus data channel
+                pc.createOffer(function (sdp) {
+                    if (sdp.sdp.indexOf('typ relay') > -1) { // sometimes sdp contains the ice candidates...
+                        promiseResolved = true;
+                        resolve(true);
+                    }
+                    pc.setLocalDescription(sdp, noop, noop);
+                }, noop); // create offer and set local description
+                pc.onicecandidate = function (ice) { //listen for candidate events
+                    if (promiseResolved || !ice || !ice.candidate || !ice.candidate.candidate || !(ice.candidate.candidate.indexOf('typ relay') > -1)) return;
+                    promiseResolved = true;
+                    resolve(true);
+                };
+            });
+        }
+
         init();
     }
 
