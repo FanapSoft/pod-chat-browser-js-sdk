80262161899d6d2368234a642d9287d1405e4aa3
wp-src <wpmagic70@gmail.com>
2022-02-01 20:09:30 +0330

    Modified createCallThreadRequest in startCall

--- src/call.module.js
+++ src/call.module.js
@@ -2604,12 +2604,10 @@
                 }
 
                 if(params.threadInfo
-                    && (params.threadInfo.title
-                        || params.threadInfo.description
-                        || params.threadInfo.metadata
+                    && (params.threadInfo.metadata
                         || params.threadInfo.uniqueName)
                 ) {
-                    startCallData.content.createCallThreadRequest = params.threadInfo
+                    content.createCallThreadRequest = params.threadInfo
                 }
 
                 startCallData.content = JSON.stringify(content);
